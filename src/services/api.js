2f228c9b8726ef6ecfdcfe3ada6e5f0ca675b8eb
DilankaSilva <136069673+DilankaSilva@users.noreply.github.com>
2025-05-11 06:12:50 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = process.env.TMDB_API_KEY;
-const BASE_URL = process.env.TMDB_BASE_URL;
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const BASE_URL = process.env.REACT_APP_TMDB_BASE_URL;
 
 const tmdbApi = axios.create({
   baseURL: BASE_URL,
